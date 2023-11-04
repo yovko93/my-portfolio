43efe3f43c20bf274938022cab92bead0e50c3fa
Yovko Garkov <iovko1993@gmail.com>
2023-11-04 23:53:38 +0200

    Add models

---
+++ MyPortfolioApp/Models/Project.cs
@@ -0,0 +1,9 @@
+﻿namespace MyPortfolioApp.Models
+{
+    public class Project
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+    }
+}
