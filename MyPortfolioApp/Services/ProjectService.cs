10ff6b172f8859630548a5623159ee1653af40fe
Yovko Garkov <iovko1993@gmail.com>
2023-11-05 22:23:40 +0200

    Fix projects

---
+++ MyPortfolioApp/Services/ProjectService.cs
@@ -0,0 +1,25 @@
+﻿using MyPortfolioApp.Models;
+using System.Net.Http.Json;
+
+namespace MyPortfolioApp.Services
+{
+    public sealed class ProjectService : IDisposable
+    {
+        private readonly HttpClient _client;
+        private readonly Task<List<Project>?> _getProjectsTask;
+
+        public ProjectService(HttpClient client)
+        {
+            _client = client;
+            _getProjectsTask = _client.GetFromJsonAsync<List<Project>>("sample-data/projects.json");
+        }
+
+        internal async Task<List<Project>> GetProjectsAsync()
+        {
+            var projects = await _getProjectsTask;
+            return projects.ToList();
+        }
+
+        public void Dispose() => _client.Dispose();
+    }
+}
