9d8b173fb68ba0a7e43a1d9af96e67a9205a7c24
Yovko Garkov <iovko1993@gmail.com>
2023-11-05 20:07:27 +0200

    Add image service

--- MyPortfolioApp/Program.cs
+++ MyPortfolioApp/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MyPortfolioApp;
+using MyPortfolioApp.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<ImageService>();
 
 await builder.Build().RunAsync();
