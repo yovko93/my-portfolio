54ebf476aa9e6162a9e91c13a5b3c59735e413bf
Yovko Garkov <iovko1993@gmail.com>
2023-12-29 16:41:35 +0200

    Move functions in separate folder

--- MyPortfolioApp/wwwroot/js/typedInterop.js
+++ MyPortfolioApp/wwwroot/js/typedInterop.js
@@ -2,5 +2,6 @@ window.initializeTyped = function (elementId) {
     var typed = new Typed('#' + elementId, {
         strings: ['Web Developer', 'FrontEnd Developer', 'DevOps Software Engineer'],
         typeSpeed: 100,
+        loop: true
     });
 };
